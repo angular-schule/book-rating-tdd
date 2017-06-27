ca08b43daa617ba71e4d7cece7ff33d5e716e7c0
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2017-06-28 01:37:46 +0200

    a nice book-rating app with some tests

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BookStoreService } from './shared/book-store.service';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { BookComponent } from './book/book.component';
+import { CreateBookComponent } from './create-book/create-book.component';
+import { BookDetailsComponent } from './book-details/book-details.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    BookComponent,
+    CreateBookComponent,
+    BookDetailsComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AppRoutingModule
+  ],
+  providers: [
+    BookStoreService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/book/book.component.spec.ts
@@ -0,0 +1,41 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BookComponent } from './book.component';
+import { Book } from 'app/shared/book';
+import { RouterTestingModule } from '@angular/router/testing';
+
+describe('BookComponent', () => {
+  let component: BookComponent;
+  let fixture: ComponentFixture<BookComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ BookComponent ],
+      imports: [
+        RouterTestingModule
+      ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BookComponent);
+    component = fixture.componentInstance;
+  });
+
+  it('should forward calls to book.rateDown', () => {
+
+    const bookMock = new Book('', '', '');
+    spyOn(bookMock, 'rateDown').and.callThrough();
+
+    component.book = bookMock;
+    component.rateDown();
+
+    expect(bookMock.rateDown).toHaveBeenCalled();
+  });
+
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
